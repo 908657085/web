8b0c6d1d4a3f699452173bc004a27c5cc1194dca
高雄辉 <gaoxionghui@sunmi.com>
2019-03-01 17:17:03 +0800

    修改docker运行脚本

--- docker_web.sh
+++ docker_web.sh
@@ -1,1 +1,1 @@
-docker run -d -p 80:80 -v $(pwd)/web/conf.d:/etc/nginx/conf.d -v $(pwd)/web/logs:/var/log/nginx -v $(pwd)/web/html:/usr/share/nginx/html --name=webTest nginx
+docker run -d -p 80:80 -v $(pwd)/conf.d:/etc/nginx/conf.d -v $(pwd)/logs:/var/log/nginx -v $(pwd)/html:/usr/share/nginx/html --name=webTest nginx
